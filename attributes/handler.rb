54d3820498a6d2702bf68c819fe18df97c5dc4f5
daraghmartin <d.martin@base2services.com>
2016-05-17 05:05:08 +1000

    update typo in attribute

--- attributes/handler.rb
+++ attributes/handler.rb
@@ -1,4 +1,4 @@
-default['base2']['automation']['handler_location'] = default['base2']['automation']['handler_location'] = platform?('windows') ? 'c:/chef/handlers' : '/etc/chef/handlers'
+default['base2']['automation']['handler_location'] = platform?('windows') ? 'c:/chef/handlers' : '/etc/chef/handlers'
 default['base2']['automation']['handlers']['slack']['deploy'] = true
 default['base2']['automation']['handlers']['slack']['web_hook_url'] = 'https://hooks.slack.com/services/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'
 default['base2']['automation']['handlers']['slack']['icon_emoji']= ':robot_face:'
