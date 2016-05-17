31587c2d1d58e492c43e8c83cbc9d0df93bad03a
daraghmartin <d.martin@base2services.com>
2016-05-17 10:00:52 +1000

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures base2-chef-handler-notifier'
 long_description 'Installs/Configures base2-chef-handler-notifier'
-version '0.1.0'
+version '0.2.0'
 
 depends 'chef_handler'
